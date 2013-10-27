4f676e0a6300dbaa378f4b5ef1a536ba278129a9
Osvaldo Brignoni <obrignoni@gmail.com>
2013-10-27 11:06:15 -0400

    Update language

--- language/english/multiple_lang.php
+++ language/english/multiple_lang.php
@@ -4,4 +4,10 @@
 $lang['streams:multiple.name'] 					= 'Multiple';
 
 // Placeholders
-$lang['streams:multiple.placeholder']			= 'Type to search...';
+$lang['streams:stream']							= 'Stream';
+$lang['streams:multiple.max_selections']		= 'Max Selections';
+$lang['streams:multiple.placeholder']			= 'Type to Search...';
+$lang['streams:multiple.label_field']			= 'Label Field';
+$lang['streams:multiple.value_field']			= 'Value Field';
+$lang['streams:multiple.template']				= 'Template';
+$lang['streams:multiple.module_slug']			= 'Module Slug';
